8a4be4e7923c780c231cacd5283c847747211cfd
Muhammad Awais <muhammadawaisanas555@gmail.com>
2026-07-12 11:27:39 +0500

    Create share.route.js

---
+++ routes/share.route.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const { protect } = require("../middleware/auth.middleware");
+const { createShareLink, resolveShareLink, claimShareLink } = require("../controllers/share.controller");
+
+router.post("/", protect, createShareLink);
+router.get("/:token", resolveShareLink);
+router.post("/:token/claim", protect, claimShareLink);
+
+module.exports = router;
