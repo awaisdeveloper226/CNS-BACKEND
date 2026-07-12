7d31970181a2b208235ea665d8f8b6077f7da23e
Muhammad Awais <muhammadawaisanas555@gmail.com>
2026-07-12 17:55:09 +0500

    Add guestUser field to businessMeta in ShareLink model

--- models/ShareLink.js
+++ models/ShareLink.js
@@ -7,6 +7,7 @@ const shareLinkSchema = new mongoose.Schema(
     placeId: { type: String, default: null }, // set instead of `business` for unregistered/global businesses
    businessMeta: {
   name: String,
+  guestUser: { type: mongoose.Schema.Types.ObjectId, ref: "User", default: null },
   address: String,
   type: { type: String },
   coordinates: { lat: Number, lng: Number },
