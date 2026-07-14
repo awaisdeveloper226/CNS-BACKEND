096f8fd0e6f449fc1df664813147e2d3639e66c8
Muhammad Awais <muhammadawaisanas555@gmail.com>
2026-07-14 12:28:24 +0500

    Update payment.routes.js

--- routes/payment.routes.js
+++ routes/payment.routes.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { createCheckoutSession } = require('../controllers/payment.controller');
+const {
+  getPriceInfo,
+  createCheckoutSession,
+  updateDriverCount,
+} = require('../controllers/payment.controller');
+const { protect } = require('../middleware/auth.middleware');
 
+router.get('/price-info', getPriceInfo);
 router.post('/create-checkout-session', createCheckoutSession);
+router.patch('/update-driver-count', protect, updateDriverCount);
 
 module.exports = router;
