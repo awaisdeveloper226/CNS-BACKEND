1dc1c7a168d82cc5a08193689698c594c38d1586
Muhammad Awais <muhammadawaisanas555@gmail.com>
2026-07-20 15:48:26 +0500

    Add cancellation OTP and confirmation routes

--- routes/payment.routes.js
+++ routes/payment.routes.js
@@ -4,11 +4,13 @@ const {
   getPriceInfo,
   createCheckoutSession,
   updateDriverCount,
+  requestCancellationOtp,
+  confirmCancellation,
 } = require('../controllers/payment.controller');
 const { protect } = require('../middleware/auth.middleware');
-
 router.get('/price-info', getPriceInfo);
 router.post('/create-checkout-session', createCheckoutSession);
 router.patch('/update-driver-count', protect, updateDriverCount);
-
+router.post('/request-cancellation-otp', protect, requestCancellationOtp);
+router.post('/confirm-cancellation', protect, confirmCancellation);
 module.exports = router;
